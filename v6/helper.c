b19499c3083c4f2a1d8eef8652138269a592ccb2
SiangYin <siangyin916@gmail.com>
2024-02-25 22:34:54 +0800

    Update helper.c

--- v6/helper.c
+++ v6/helper.c
@@ -1,8 +1,10 @@
 #include <xc.h>
 #include "config.h"
 
+// Function to determine the index of the tone based on the current count
 unsigned int helper_getToneIndex(unsigned int currCount) {
     switch (currCount) {
+        // Cases for specific counts corresponding to different tones
         case 3:
         case 6:
         case 7:
@@ -15,8 +17,7 @@ unsigned int helper_getToneIndex(unsigned int currCount) {
         case 20:
         case 21:
         case 25:
-            return 2;
-
+            return 2; // Return index 2 for specific counts
         case 2:
         case 4:
         case 5:
@@ -27,15 +28,12 @@ unsigned int helper_getToneIndex(unsigned int currCount) {
         case 18:
         case 22:
         case 24:
-            return 1;
-
+            return 1; // Return index 1 for specific counts
         case 1:
         case 10:
         case 23:
-            return 0;
-
+            return 0; // Return index 0 for specific counts
+        default:
+            return 0; // Return default index 0 for unknown counts
     }
- return 0;
-
 }
-
